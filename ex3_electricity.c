87affd8e5b9ccfa08c6d9c15d86036bccfd679d9
Pius-Emma <rwotomiapiusemmanuel04@gmail.com>
2026-03-24 22:33:24 +0300

    Implement electricity bill calculation in main function
    Updated author information and implemented main function to calculate electricity bill.

--- ex3_electricity.c
+++ ex3_electricity.c
@@ -1,11 +1,17 @@
-/* Name: Your Full Name */
-/* Student Number: Your Registration Number */
+/* Name: RWOTOMIA PIUSEMMANUEL*/
+/* Student Number: 25/U/BIE/01419/PE */
 
 #include <stdio.h>
-
-int main(void)
-{
-    /* Write your code here */
+int main() {
+    float unitsConsumed,costPerUnit,totalElectricityBill; 
+    printf("Enter units Consumed: ");
+    scanf("%f",&unitsConsumed); 
+    printf("Enter cost per unit:");
+    scanf("%f",&costPerUnit); 
+    totalElectricityBill = unitsConsumed * costPerUnit;
+    printf("......TRANSACTION SUMMARY......");
+    printf("\nTotal electricity bill: %.2f UGX\n",totalElectricityBill);
+    printf("....................");
 
     return 0;
 }
