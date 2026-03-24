5c08894e4e6de42e7c547bfb2b413adc98910996
Pius-Emma <rwotomiapiusemmanuel04@gmail.com>
2026-03-24 20:19:07 +0300

    Implement transaction fee calculation in mobile money
    Added functionality to calculate transaction fee and total amount deducted.

--- ex1_mobile_money.c
+++ ex1_mobile_money.c
@@ -1,11 +1,22 @@
-/* Name: Your Full Name */
-/* Student Number: Your Registration Number */
+/* Name: RWOTOMIA PIUS EMMANUEL*/
+/* Student Number: 25/U/BIE/01419/PE */
 
 #include <stdio.h>
 
 int main(void)
 {
-    /* Write your code here */
+    float amountToSend,transactionFee,feeAmount,totalAmountDeducted;
+    printf("Enter amount to send:");
+    scanf("%f",&amountToSend); 
+    printf("Enter transaction fee:");
+    scanf("%f",&transactionFee); 
+    feeAmount = (transactionFee/ 100) * amountToSend;
+    totalAmountDeducted = feeAmount + amountToSend; 
+    printf("......TRANSACTION SUMMARY.... .");
+    printf("\nAmount sent: %.2f UGX\n",amountToSend); 
+    printf("\nTransaction fee: %.2f UGX\n",feeAmount); 
+    printf("\nTotal amount deducted: %.2f UGX\n",totalAmountDeducted);
+    printf("...................");
 
     return 0;
 }
